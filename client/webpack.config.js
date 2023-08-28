03447051124b02fb068c75e528ed792ab87fec71
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-28 11:31:51 -0700

    webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = () => {
@@ -10,18 +11,38 @@ module.exports = () => {
     },
 
     // TODO: Add the correct output
-    output: {
-      
+    output: { 
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contact-Card'
+      }),
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
     }
   };
 };
