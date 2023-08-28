bf3f2ce8878f4e05b2223cd35d87870fe6ed91bb
unknown <lukeharriman1013@gmail.com>
2023-08-28 11:24:52 -0700

    databse and worker

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,1 +1,10 @@
 // TODO: Create a service worker that caches static assets:
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  }
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -2,15 +2,33 @@
 import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
-const initdb = async () => {
-
-};
+const initDb = async () => {
+    try {
+      // Open a database (create it if it doesn't exist)
+      const db = await openDB('YourDatabaseName', 1, {
+        upgrade(db) {
+          // Create object stores or perform any database schema upgrades here
+          if (!db.objectStoreNames.contains('yourObjectStoreName')) {
+            const store = db.createObjectStore('yourObjectStoreName', {
+              keyPath: 'id', //  primary key
+              autoIncrement: true, // auto-generated keys
+            });
+          }
+        },
+      });
+  
+      console.log('Database initialized successfully', db);
+      return db; // You can return the database instance if needed
+    } catch (error) {
+      console.error('Error initializing database', error);
+      throw error;
+    }
+  };
 
 
 // TODO: Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
- 
-};
+    
 
 // TODO: Complete the getDb() function below:
 export const getDb = async () => {
@@ -22,4 +40,4 @@ export const deleteDb = async (id) => {
   
 };
 
-initdb();
+initDb();
